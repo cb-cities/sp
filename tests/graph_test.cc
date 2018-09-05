f2ab168050aa4798ee200449f64bffcee21bbd8c
Krishna Kumar <3963513+kks32@users.noreply.github.com>
2018-09-05 11:57:22 +0100

    :dart: Test Read Matrix Market file

--- tests/graph_test.cc
+++ tests/graph_test.cc
@@ -73,4 +73,25 @@ TEST_CASE("Graph is checked", "[graph][od]") {
       REQUIRE(distances.at(3) == Approx(9.1).epsilon(Tolerance));
     }
   }
+
+  SECTION("Test SSSP in directed graph from file") {
+    // Set graph properties
+    const bool directed = true;
+
+    // Create graph object
+    auto graph = std::make_unique<Graph>(directed);
+    // Read MatrixMarket file
+    const std::string filename = "../sf.mtx";
+    graph->read_graph_matrix_market(filename);
+
+    // Run Dijkstra Priority Queue
+    Graph::vertex_t source = 1;
+    Graph::vertex_t destination = 3;
+    auto distances = graph->dijkstra_priority_queue(source, destination);
+
+    // Check distances
+    REQUIRE(distances.size() == graph->nvertices());
+    // Check shortest path
+    REQUIRE(distances.at(3) == Approx(12756.7622297699).epsilon(Tolerance));
+  }
 }
