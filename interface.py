76eb0d0b3fecf78af5d84b9163159c32d5f30389
Jeffrey Salmond <js947@cam.ac.uk>
2018-09-07 11:36:39 +0100

    :construction: rework python interface to be more classy

--- interface.py
+++ interface.py
@@ -3,16 +3,39 @@ from __future__ import print_function
 from sys import argv
 from ctypes import *
 
-py_main = cdll.LoadLibrary("./liblsp.so").py_main
+libsp = cdll.LoadLibrary("./liblsp.so")
+
+class Graph(Structure):
+    def dijkstra(self, origin, destination):
+        sp = ShortestPath()
+        libsp.shortestpath(byref(sp), byref(self), origin, destination)
+        return sp
+
+
+libsp.simplegraph.restype = POINTER(Graph)
+
+def simplegraph(directed=True):
+    return libsp.simplegraph(directed).contents
+
+libsp.readgraph.restype = POINTER(Graph)
+
+def readgraph(filename, directed=True):
+    return libsp.readgraph(filename, directed).contents
+
 
 class ShortestPath(Structure):
     _fields_ = [("destination", c_int),
                 ("distance", c_double)]
 
-sp = ShortestPath()
+    pass
 
-filename = "../sf.mtx"
-py_main(byref(sp), filename, 1, 3)
 
-print(sp.destination)
-print(sp.distance)
+def test():
+    g = simplegraph()
+    sp = g.dijkstra(1, 3)
+
+    print(sp.destination)
+    print(sp.distance)
+
+if __name__ == '__main__':
+    test()
--- src/py.cc
+++ src/py.cc
@@ -8,19 +8,22 @@ extern "C" {
     double distance;
   };
 
-
-  int py_main(ShortestPath_py* ret, const char* filename, int origin, int destination) {
-    std::cout << "filename: " << filename << std::endl;
-
-    const bool directed = true;
-    auto graph = std::make_unique<Graph>(directed);
-    //graph->read_graph_matrix_market(filename);
+  Graph* simplegraph(bool directed) {
+    Graph* graph = new Graph(directed);
     graph->generate_simple_graph();
-    const auto sp = graph->dijkstra_priority_queue(origin, destination);
+    return graph;
+  }
 
+  Graph* readgraph(char* filename, bool directed) {
+    Graph* graph = new Graph(directed);
+    graph->read_graph_matrix_market(filename);
+    return graph;
+  }
+
+  int shortestpath(ShortestPath_py* ret, Graph* graph, int origin, int destination) {
+    const auto sp = graph->dijkstra_priority_queue(origin, destination);
     ret->destination = destination;
     ret->distance = sp.distances[destination];
-
     return 0;
   }
 }
